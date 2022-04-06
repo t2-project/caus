2aad45ba21035e50fc7584496247b8d904f3f464
Markus Z <st114857@stud.uni.stuttgart.de>
2022-04-06 15:26:25 +0200

    default code for kubernetes client

--- controller.py
+++ controller.py
@@ -1,5 +1,16 @@
 from caus import CAUS
+from kubernetes import client, config
 
-print("Testcontroller")
+print("Loading kube config")
+config.load_kube_config()
+
+#list all pods / test code
+v1 = client.CoreV1Api()
+print("Listening pods with their IPs:")
+ret = v1.list_pod_for_all_namespaces(watch=False)
+for i in ret.items:
+    print("%s\%t\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+#causstuff
 myCaus = CAUS()
 print(myCaus.elasticityCapacity)
