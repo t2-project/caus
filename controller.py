c3ea5fac15e0e90a67c86639078100b3343da340
Markus Z <st114857@stud.uni.stuttgart.de>
2022-04-07 12:26:56 +0200

    typo fix, first working example (only in correct network/vpn)

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ from caus import CAUS
 from kubernetes import client, config
 
 print("Load kube config")
-os.environ['KUBECONFIG'] = './k8s-cluster3-admin.conf'
+#os.environ['KUBECONFIG'] = './k8s-cluster3-admin.conf'
 #try:
 #    config.load_incluster_config()
 #except config.ConfigException:
@@ -15,7 +15,7 @@ v1 = client.CoreV1Api()
 print("Listening pods with their IPs:")
 ret = v1.list_pod_for_all_namespaces(watch=False)
 for i in ret.items:
-    print("%s\%t\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
 #causstuff
 myCaus = CAUS()
