d7cb1bf2039f8d7074588bfd95aa1745aafbb9e3
Markus Z <st114857@stud.uni.stuttgart.de>
2022-04-07 14:24:41 +0200

    example code for finding namespaces

--- controller.py
+++ controller.py
@@ -10,12 +10,22 @@ print("Load kube config")
 #    config.load_kube_config()
 config.load_kube_config(config_file='k8s-cluster3-admin.conf')
 
-#list all pods / test code
 v1 = client.CoreV1Api()
-print("Listening pods with their IPs:")
-ret = v1.list_pod_for_all_namespaces(watch=False)
+
+#list all pods / test code
+#print("Listening pods with their IPs:")
+#ret = v1.list_pod_for_all_namespaces(watch=False)
+#for i in ret.items:
+#    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+#list all namespaces / test code
+print("Finding namespaces: ...")
+ret = v1.list_namespace()
 for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+# code for finding deployments in a namespace, does not work with corev1api -> needs rest api of kubernetes python client
+#    depl_ret = v1.list_namespaced_deployment(namespace=i.metadata.name)
+#    for i in depl_ret.items:
+    print(i.metadata.name)
 
 #causstuff
 myCaus = CAUS()
