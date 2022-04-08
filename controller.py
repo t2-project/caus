9d2745ef366a2d5214afa1a92a40528f6fcaaaac
Markus Z <st114857@stud.uni.stuttgart.de>
2022-04-08 15:41:09 +0200

    deployment example code

--- controller.py
+++ controller.py
@@ -2,31 +2,155 @@ import os
 from caus import CAUS
 from kubernetes import client, config
 
-print("Load kube config")
-#os.environ['KUBECONFIG'] = './k8s-cluster3-admin.conf'
-#try:
-#    config.load_incluster_config()
-#except config.ConfigException:
-#    config.load_kube_config()
-config.load_kube_config(config_file='k8s-cluster3-admin.conf')
 
-v1 = client.CoreV1Api()
+DEPLOYMENT_NAME = "nginx-deployment"
 
-#list all pods / test code
-#print("Listening pods with their IPs:")
-#ret = v1.list_pod_for_all_namespaces(watch=False)
-#for i in ret.items:
-#    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
 
-#list all namespaces / test code
-print("Finding namespaces: ...")
-ret = v1.list_namespace()
-for i in ret.items:
-# code for finding deployments in a namespace, does not work with corev1api -> needs rest api of kubernetes python client
-#    depl_ret = v1.list_namespaced_deployment(namespace=i.metadata.name)
-#    for i in depl_ret.items:
-    print(i.metadata.name)
+def create_deployment_object():
+    # Configureate Pod template container
+    container = client.V1Container(
+        name="nginx",
+        image="nginx:1.15.4",
+        ports=[client.V1ContainerPort(container_port=80)],
+        resources=client.V1ResourceRequirements(
+            requests={"cpu": "100m", "memory": "200Mi"},
+            limits={"cpu": "500m", "memory": "500Mi"},
+        ),
+    )
 
-#causstuff
-myCaus = CAUS()
-print(myCaus.elasticityCapacity)
+    # Create and configure a spec section
+    template = client.V1PodTemplateSpec(
+        metadata=client.V1ObjectMeta(labels={"app": "nginx"}),
+        spec=client.V1PodSpec(containers=[container]),
+    )
+
+    # Create the specification of deployment
+    spec = client.V1DeploymentSpec(
+        replicas=3, template=template, selector={
+            "matchLabels":
+            {"app": "nginx"}})
+
+    # Instantiate the deployment object
+    deployment = client.V1Deployment(
+        api_version="apps/v1",
+        kind="Deployment",
+        metadata=client.V1ObjectMeta(name=DEPLOYMENT_NAME),
+        spec=spec,
+    )
+
+    return deployment
+
+def create_deployment(api, deployment):
+    # Create deployement
+    resp = api.create_namespaced_deployment(
+        body=deployment, namespace="default"
+    )
+
+    print("\n[INFO] deployment `nginx-deployment` created.\n")
+    print("%s\t%s\t\t\t%s\t%s" % ("NAMESPACE", "NAME", "REVISION", "IMAGE"))
+    print(
+        "%s\t\t%s\t%s\t\t%s\n"
+        % (
+            resp.metadata.namespace,
+            resp.metadata.name,
+            resp.metadata.generation,
+            resp.spec.template.spec.containers[0].image,
+        )
+    )
+
+def update_deployment(api, deployment):
+    # Update container image
+    deployment.spec.template.spec.containers[0].image = "nginx:1.16.0"
+
+    # patch the deployment
+    resp = api.patch_namespaced_deployment(
+        name=DEPLOYMENT_NAME, namespace="default", body=deployment
+    )
+
+    print("\n[INFO] deployment's container image updated.\n")
+    print("%s\t%s\t\t\t%s\t%s" % ("NAMESPACE", "NAME", "REVISION", "IMAGE"))
+    print(
+        "%s\t\t%s\t%s\t\t%s\n"
+        % (
+            resp.metadata.namespace,
+            resp.metadata.name,
+            resp.metadata.generation,
+            resp.spec.template.spec.containers[0].image,
+        )
+    )
+
+def restart_deployment(api, deployment):
+    # update `spec.template.metadata` section
+    # to add `kubectl.kubernetes.io/restartedAt` annotation
+    deployment.spec.template.metadata.annotations = {
+        "kubectl.kubernetes.io/restartedAt": datetime.datetime.utcnow()
+        .replace(tzinfo=pytz.UTC)
+        .isoformat()
+    }
+
+    # patch the deployment
+    resp = api.patch_namespaced_deployment(
+        name=DEPLOYMENT_NAME, namespace="default", body=deployment
+    )
+
+    print("\n[INFO] deployment `nginx-deployment` restarted.\n")
+    print("%s\t\t\t%s\t%s" % ("NAME", "REVISION", "RESTARTED-AT"))
+    print(
+        "%s\t%s\t\t%s\n"
+        % (
+            resp.metadata.name,
+            resp.metadata.generation,
+            resp.spec.template.metadata.annotations,
+        )
+    )
+
+def delete_deployment(api):
+    # Delete deployment
+    resp = api.delete_namespaced_deployment(
+        name=DEPLOYMENT_NAME,
+        namespace="default",
+        body=client.V1DeleteOptions(
+            propagation_policy="Foreground", grace_period_seconds=5
+        ),
+    )
+    print("\n[INFO] deployment `nginx-deployment` deleted.")
+
+def list_pods(api):
+    print("Listing pods with their IPs:")
+    ret = api.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+def main():
+    print("Load kube config")
+    #os.environ['KUBECONFIG'] = './k8s-cluster3-admin.conf'
+    #try:
+    #    config.load_incluster_config()
+    #except config.ConfigException:
+    #    config.load_kube_config()
+    config.load_kube_config(config_file='k8s-cluster3-admin.conf')
+
+    #get core and apps api
+    core_api = client.CoreV1Api()
+    apis_api = client.AppsV1Api()
+
+    list_pods(core_api)
+
+    deployment = create_deployment_object()
+
+#    create_deployment(apis_api, deployment)
+#    delete_deployment(apis_api)
+#    list_pods(core_api)
+
+    #list all namespaces / test code
+    #print("Finding namespaces: ...")
+    #ret = core_api.list_namespace()
+    #for i in ret.items:
+    #    print(i.metadata.name)
+
+    #causstuff
+    myCaus = CAUS()
+    print(myCaus.elasticityCapacity)
+
+if __name__ == "__main__":
+    main()
