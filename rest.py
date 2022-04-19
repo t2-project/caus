e623ce8e8cd257fd435508c1677bd2c2ce0c56ab
Markus Z <st114857@stud.uni.stuttgart.de>
2022-04-19 16:46:41 +0200

    added endpoints for ml method decisions and explainability decisions

--- rest.py
+++ rest.py
@@ -1,22 +1,24 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        return update_stuff()
-    if request.method == 'GET':
-        return build_start_page()
+    return build_start_page()
 
 @app.route('/elasticity')
-def say_hello():
+def returnElasticity():
     return 'Hello from Server'
 
+@app.route('/scalingmethod',methods=['GET','POST'])
+def handle_scalingmethods():
+    if request.method == 'GET':
+        return 'Return possible scaling methods here'
+    else:
+        return 'Change scaling method to sent method'
 
-def update_stuff():
-    a = 1
-    return str(a)
-
-def build_start_page():
-    a = 0
-    return str(a)
+@app.route('/explainability', methods=['GET', 'POST'])
+def handle_explainability():
+    if request.method == 'GET':
+        return 'return possible explainability methods here (depending on the machine learning method'
+    else:
+        return 'change explainability method to chosen method'
