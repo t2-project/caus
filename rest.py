fa5b566ad771387c5366919d7cc8adf316ddd80f
Markus Z <st114857@stud.uni.stuttgart.de>
2022-04-25 14:16:12 +0200

    pushed everything into own classes and tried to build a multiprocessing service

--- rest.py
+++ rest.py
@@ -1,28 +1,30 @@
 from flask import Flask, request
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return build_start_page()
+class API:
+    app = Flask(__name__)
 
-@app.route('/elasticity')
-def returnElasticity():
-    return 'Hello from Server'
+    @app.route('/')
+    def index():
+        return self.build_start_page()
 
-@app.route('/scalingmethod',methods=['GET','POST'])
-def handle_scalingmethods():
-    if request.method == 'GET':
-        return 'Return possible scaling methods here'
-    else:
-        return 'Change scaling method to sent method'
+    @app.route('/elasticity')
+    def returnElasticity():
+        return 'Hello from Server'
 
-@app.route('/explainability', methods=['GET', 'POST'])
-def handle_explainability():
-    if request.method == 'GET':
-        return 'return possible explainability methods here (depending on the machine learning method'
-    else:
-        return 'change explainability method to chosen method'
+    @app.route('/scalingmethod',methods=['GET','POST'])
+    def handle_scalingmethods():
+        if request.method == 'GET':
+            return 'Return possible scaling methods here'
+        else:
+            return 'Change scaling method to sent method'
 
-@app.route('/explainresult')
-def return_explainability_result:
-    return 'return explainability result for chosen explainability method'
+    @app.route('/explainability', methods=['GET', 'POST'])
+    def handle_explainability():
+        if request.method == 'GET':
+            return 'return possible explainability methods here (depending on the machine learning method'
+        else:
+            return 'change explainability method to chosen method'
+
+    @app.route('/explainresult')
+    def return_explainability_result():
+        return 'return explainability result for chosen explainability method'
